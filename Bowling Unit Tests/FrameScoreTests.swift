23fdd1c4128cfb4a15ce98bdb47cfec362bb4fea
Sam Carr <sam.carr@uk.tesco.com>
2019-03-23 23:08:57 +0000

    Move cases into the method in FrameScoreTests, and lose the names in the tuple as the line was crazy long and they were effectively repeated.

--- Bowling Unit Tests/FrameScoreTests.swift
+++ Bowling Unit Tests/FrameScoreTests.swift
@@ -1,21 +1,20 @@
 import XCTest
 
-fileprivate let cases:
-    [(ball1: Int, ball2: Int?, subsequentBalls: [Int], expectedScore: Int, line: UInt)] = [
-//   b1  b2   subsBalls score
-    (4,  nil, [],       4,    #line),
-    (4,  5,   [],       9,    #line),
-    (3,  7,   [],       10,   #line),
-    (3,  7,   [4],      14,   #line),
-    (3,  7,   [4, 3],   14,   #line),
-    (10, nil, [],       10,   #line),
-    (10, nil, [5],      15,   #line),
-    (10, nil, [5, 4],   19,   #line),
-    (10, 10,  [10],     30,   #line)]
-
 class FrameScoreTests: XCTestCase {
 
     func testFrameScores() {
+        let cases: [(Int, Int?, [Int], Int, UInt)] = [
+//           b1  b2   subsBalls score
+            (4,  nil, [],       4,    #line),
+            (4,  5,   [],       9,    #line),
+            (3,  7,   [],       10,   #line),
+            (3,  7,   [4],      14,   #line),
+            (3,  7,   [4, 3],   14,   #line),
+            (10, nil, [],       10,   #line),
+            (10, nil, [5],      15,   #line),
+            (10, nil, [5, 4],   19,   #line),
+            (10, 10,  [10],     30,   #line)]
+
         cases.forEach { (ball1, ball2, subsequentBalls, expectedScore, line) in
             let frame = Frame(ball1: ball1, ball2: ball2, subsequentBalls: subsequentBalls)
             XCTAssertEqual(frame.score, expectedScore, line: line)
